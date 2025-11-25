c06f7df8db3712a09535632f1fe9826af64d99cf
senecobis <ro.pellerito@libero.it>
2025-11-25 11:11:08 +0000

    general refactoring

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('requirement.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="suppressor",
+    name="ev-loader",
     version="0.1.0",  # Update version as needed
     author="Your Name",
     author_email="rpellerito@ifi.uzh.ch",
-    description="A Python package for suppressing events and loading datasets",
+    description="A Python package for loading datasets",
     long_description=open("README.md").read(),
-    url="https://github.com/senecobis/suppressor",  # Replace with your repo URL
+    url="https://github.com/senecobis/ev-loader",  # Replace with your repo URL
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
