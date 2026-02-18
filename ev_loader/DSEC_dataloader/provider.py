3a1266b4b7a59be84e27df642b25f538c178c8ef
senecobis <ro.pellerito@icloud.com>
2026-02-18 18:11:06 +0100

    Add method to retrieve detection test dataset in DatasetProvider

--- ev_loader/DSEC_dataloader/provider.py
+++ ev_loader/DSEC_dataloader/provider.py
@@ -249,6 +249,18 @@ class DatasetProvider:
                                             ))
         return torch.utils.data.ConcatDataset(train_sequences)
     
+    def get_detection_test_dataset(self, num_events: int = 0):
+        assert self.test_path.is_dir(), str(self.test_path)
+        test_sequences = list()
+        for child in sorted(self.test_path.iterdir()):
+            test_sequences.append(DetSequence(seq_path=child, 
+                                            mode='test', 
+                                            num_bins=self.num_bins, 
+                                            representation=self.representation,
+                                            num_events=num_events,
+                                            ))
+        return test_sequences
+    
 if __name__ == "__main__":
     dsec_dir = "/data/scratch/pellerito/datasets/DSEC"
     num_bins = 15
